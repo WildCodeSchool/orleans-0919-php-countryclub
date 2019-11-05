0cd097f961b1ddbcd23fb9ee8be54dcbea0ac1bd
Valentin Fauchère <valentin.fauchere@gmail.com>
2019-11-05 15:07:57 +0100

    Add news home html and controllers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,8 @@
 
 namespace App\Controller;
 
+use App\Model\NewsManager;
+
 class HomeController extends AbstractController
 {
 
@@ -21,6 +23,9 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->twig->render('Home/index.html.twig');
+
+        $NewsManager = new NewsManager();
+        $news = $NewsManager->selectAll();
+        return $this->twig->render('Home/index.html.twig', ['news' =>$news]);
     }
 }
