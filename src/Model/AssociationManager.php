17402e4c18ecbaa2ef66e2afef4e9da7032ce66e
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2019-11-06 15:37:22 +0100

    page asso ajout bureau presentation

---
+++ src/Model/AssociationManager.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class AssociationManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'members';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+}
+
