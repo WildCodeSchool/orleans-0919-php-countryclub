11562976cf1f7eebce651cf310b80ea295396cf3
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2019-11-14 17:36:06 +0100

    formulaire verif OK page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,7 +16,6 @@ class ContactController extends AbstractController
             if (empty($errors)) {
                 header('Location: /Contact/index/?success=ok');
             }
-
         }
         return $this->twig->render('Contact/index.html.twig', [
             'errors' => $errors ?? [],
@@ -24,47 +23,60 @@ class ContactController extends AbstractController
         ]);
     }
 
-
-    public function validate($data)
+    private function validate($data)
     {
-
+        $emptyErrors = $this->validateEmpty($data);
         $errors = [];
 
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $data = array_map('trim', $_POST);
-
-            if (empty($data['lastname'])) {
-                $errors['lastname'] = 'Le nom doit être indiqué';
-            } elseif (strlen($data['lastname']) > 155) {
-                $errors['lastname'] = 'Le nom est trop long';
-            }
-            if (empty($data['firstname'])) {
-                $errors['firstname'] = 'Le prénom doit être indiqué';
-            } elseif (strlen($data['firstname']) > 155) {
-                $errors['firstname'] = 'Le prénom est trop long';
-            }
-            if (empty($data['mail'])) {
-                $errors['mail'] = 'Le mail doit être précisé';
-            } elseif (strlen($data['mail']) > 50) {
-                $errors['mail'] = 'Le mail est trop long';
-            }
-            if (!filter_var($data['mail'], FILTER_VALIDATE_EMAIL)) {
-                $errors['mail'] = "Votre mail est invalide";
-            }
-            if (empty($data['subject'])) {
-                $errors['subject'] = 'Votre sujet doit être mentionné';
-            } elseif (strlen($data['subject']) > 155) {
-                $errors['subject'] = 'Votre sujet est trop long';
-            }
-            if (empty($data['msg'])) {
-                $errors['msg'] = 'Votre message doit être mentionné';
-            } elseif (strlen($data['msg']) > 255) {
-                $errors['msg'] = 'Votre message est trop long';
-            }
-
+        if (strlen($data['lastname']) > 155) {
+            $errors['lastname'] = 'Le nom est trop long';
         }
-        return $errors;
+
+        if (strlen($data['firstname']) > 155) {
+            $errors['firstname'] = 'Le prénom est trop long';
+        }
+
+        if (strlen($data['mail']) > 50) {
+            $errors['mail'] = 'Le mail est trop long';
+        }
+
+        if (!filter_var($data['mail'], FILTER_VALIDATE_EMAIL)) {
+            $errors['mail'] = "Votre mail est invalide";
+        }
+
+        if (strlen($data['subject']) > 155) {
+            $errors['subject'] = 'Votre sujet est trop long';
+        }
+
+        if (strlen($data['msg']) > 255) {
+            $errors['msg'] = 'Votre message est trop long';
+        }
+
+        return array_merge($emptyErrors, $errors);
     }
 
+    private function validateEmpty(array $data)
+    {
+        if (empty($data['lastname'])) {
+            $errors['lastname'] = 'Le nom doit être indiqué';
+        }
+
+        if (empty($data['firstname'])) {
+            $errors['firstname'] = 'Le prénom doit être indiqué';
+        }
+
+        if (empty($data['mail'])) {
+            $errors['mail'] = 'Le mail doit être précisé';
+        }
+
+        if (empty($data['subject'])) {
+            $errors['subject'] = 'Votre sujet doit être mentionné';
+        }
+
+        if (empty($data['msg'])) {
+            $errors['msg'] = 'Votre message doit être mentionné';
+        }
+
+        return $errors ?? [];
+    }
 }
