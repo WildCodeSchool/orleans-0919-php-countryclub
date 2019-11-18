49aad0e4b477c5376ad4c00bfab9dc9898903a6c
Valentin Fauchère <valentin.fauchere@gmail.com>
2019-11-18 11:54:55 +0100

    Ajout de la fonctionalité update des news

---
+++ src/Controller/AdminNewsController.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 11/10/17
+ * Time: 16:07
+ * PHP version 7
+ */
+
+namespace App\Controller;
+
+use App\Model\NewsManager;
+
+/**
+ * Class AdminNewsController
+ *
+ */
+class AdminNewsController extends AbstractController
+{
+
+
+    /**
+     * Display news listing
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        $newsManager = new NewsManager();
+        $news = $newsManager->selectAll();
+
+        return $this->twig->render('Admin/News/index.html.twig', ['news' => $news]);
+    }
+    public function edit(int $id): string
+    {
+        $newsManager = new NewsManager();
+        $news = $newsManager->selectOneById($id);
+
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = array_map('trim', $_POST);
+            $errors = $this->validate($data);
+            if (empty($errors)) {
+                $newsManager->update($data);
+                header('Location: /AdminNews/edit/' . $data['id'] . '/?success=ok');
+            }
+
+
+        }
+
+        return $this->twig->render('Admin/News/edit.html.twig', [
+            'news' => $news,
+            'errors' => $errors ?? [],
+            'success' => $_GET['success'] ?? null,
+        ]);
+    }
+    private function validate($data)
+    {
+        $emptyErrors = $this->validateEmpty($data);
+        $errors = [];
+        if (strlen($data['title']) > 155) {
+            $errors['titre'] = 'Le titre est trop long';
+        }
+        return array_merge($emptyErrors, $errors);
+    }
+
+    private function validateEmpty(array $data)
+    {
+        if (empty($data['title'])) {
+            $errors['titre'] = 'Le titre doit être indiqué';
+        }
+        if (empty($data['description'])) {
+            $errors['description'] = 'Veuillez saisir un texte';
+        }
+        return $errors ?? [];
+    }
+
+}
+
--- src/Model/NewsManager.php
+++ src/Model/NewsManager.php
@@ -17,4 +17,20 @@ class NewsManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
+    public function update(array $news):bool
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("
+                                UPDATE " . self::TABLE . "
+                                SET `title` = :title, 
+                                `description` = :description,
+                                `date` = :date
+                                WHERE id=:id");
+        $statement->bindValue('id', $news['id'], \PDO::PARAM_INT);
+        $statement->bindValue('title', $news['title'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $news['description'], \PDO::PARAM_STR);
+        $statement->bindValue('date', $news['date'], \PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
 }
