e5f51f6049cb66aa38175d48e110ef5702e1d1f7
Emilien De Macedo <emilien45100@gmail.com>
2019-11-18 14:14:54 +0100

    Affichage des photos dans la partie admin

--- src/Controller/AdminGalleryController.php
+++ src/Controller/AdminGalleryController.php
@@ -3,15 +3,17 @@
 
 namespace App\Controller;
 
+
 use App\Model\GalleryManager;
 
-class GalleryController extends AbstractController
+class AdminGalleryController extends AbstractController
 {
     public function index()
     {
         $galleryManager = new GalleryManager();
         $photos = $galleryManager->selectAll();
-        return $this->twig->render('Gallery/index.html.twig', ['photos' => $photos]);
+
+        return $this->twig->render('Admin/Picture/index.html.twig', ['photos' => $photos]);
     }
 
 }
