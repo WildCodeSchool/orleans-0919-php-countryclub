c174641ae252c3c2f507db8abb36e21716a8f628
korede lacici <lafical@yahoo.fr>
2019-11-19 23:54:47 +0100

    CRUD edit done

--- src/Controller/AdminCountryDayController.php
+++ src/Controller/AdminCountryDayController.php
@@ -52,4 +52,30 @@ class AdminCountryDayController extends AbstractController
         }
         return $errors ?? [];
     }
+
+
+    public function edit(int $id): string
+    {
+        $adminCountryDay = new AdminCountryDayManager();
+        $event = $adminCountryDay->selectOneById($id);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = array_map('trim', $_POST);
+            $errors = $this->validate($data);
+            if (empty($errors)) {
+                if ($_FILES['file']['size'] <= $data['MAX_FILE_SIZE']) {
+                    $odlDest = $_FILES['file']['tmp_name'];
+                    $newDest = 'assets/upload/'.$_FILES['file']['name'];
+                    move_uploaded_file($odlDest, $newDest);
+                }
+                $adminCountryDay->update($data);
+                header('Location: /AdminCountryDay/index');
+            }
+        }
+        return $this->twig->render('/CountryDay/edit.html.twig', [
+            'data' => $event,
+            'errors' => $errors ?? [],
+            'success' => $_GET['success'] ?? null,
+        ]);
+    }
 }
