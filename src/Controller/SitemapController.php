6f25cb390924c6eec673786b546f54f879923b9b
Valentin Fauchère <valentin.fauchere@gmail.com>
2019-11-19 15:51:24 +0100

    Ajout de la page plan du site

---
+++ src/Controller/SitemapController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class SitemapController extends AbstractController
+{
+
+    /**
+     * Display sitemap page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+
+    public function index()
+    {
+
+        return $this->twig->render('sitemap/index.html.twig');
+    }
+}
+
